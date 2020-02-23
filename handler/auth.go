05d1be69f6a578342737af5987554e37ac7c254b
dionomusuko <ruby.school114@gmail.com>
2020-02-23 14:20:32 +0900

    Add sign-in sign-up

---
+++ handler/auth.go
@@ -0,0 +1,88 @@
+package handler
+
+import (
+	"github.com/dgrijalva/jwt-go"
+	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
+	"net/http"
+	"time"
+)
+
+type jwtCustomClaims struct {
+	UID  int    `json:"uid"`
+	Name string `json:"name"`
+	jwt.StandardClaims
+}
+
+var singingKey = []byte("secret")
+
+var Config = middleware.JWTConfig{
+	Claims:     &jwtCustomClaims{},
+	SigningKey: singingKey,
+}
+
+func Signup(c echo.Context) error {
+	user := new(model.User)
+	if err := c.Bind(user); err != nil {
+		return err
+	}
+
+	if user.Name == "" || user.Password == "" {
+		return &echo.HTTPError{
+			Code:    http.StatusBadRequest,
+			Message: "incorrect name or password",
+		}
+	}
+
+	if u := model.FindUser(&model.User{Name: user.Name}); u.ID != 0 {
+		return &echo.HTTPError{
+			Code:    http.StatusConflict,
+			Message: "name already exists",
+		}
+	}
+
+	model.CreateUser(user)
+	user.Password = ""
+
+	return c.JSON(http.StatusCreated, user)
+}
+
+func Login(c echo.Context) error {
+	u := new(model.User)
+	if err := c.Bind(u); err != nil {
+		return err
+	}
+
+	user := model.FindUser(&model.User{Name: u.Name})
+	if user.ID == 0 || user.Password != u.Password {
+		return &echo.HTTPError{
+			Code:    http.StatusUnauthorized,
+			Message: "incorrect name or password",
+		}
+	}
+
+	claims := &jwtCustomClaims{
+		user.ID,
+		user.Name,
+		jwt.StandardClaims{
+			ExpiresAt: time.Now().Add(time.Hour * 72).Unix(),
+		},
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	t, err := token.SignedString(singingKey)
+	if err != nil {
+		return err
+	}
+
+	return c.JSON(http.StatusOK, map[string]string{
+		"token": t,
+	})
+}
+
+func userIDFromToken(c echo.Context) int {
+	user := c.Get("user").(*jwt.Token)
+	claims := user.Claims.(*jwtCustomClaims)
+	uid := claims.UID
+	return uid
+}
